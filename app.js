c8ee6ec1ab3d114459bba207ed8825260404540f
poi <poivengsuon@yahoo.com>
2015-09-04 11:13:08 -0500

    Adding Time and Date

--- app.js
+++ app.js
@@ -5,7 +5,24 @@ $(document).ready(function() {
     $('body').hide().fadeIn(2000);    
 });
 
+//Date and Time 
 function printToday() {
     var today = new Date();
     document.write(today.toDateString());
+}
+
+function startTime() {
+    var today=new Date();
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = h+":"+m+":"+s;
+    var t = setTimeout(function(){startTime()},500);
+}
+
+function checkTime(i) {
+    if (i<10) {i = "0" + i}; 
+    return i;
 }
